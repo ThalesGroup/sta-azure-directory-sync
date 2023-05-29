36b985ed82c380cafb4fbdad452b3900f3ca886b
zimam <md-zayed.imam@thalesgroup.com>
2023-05-29 13:00:33 -0400

    Update From Run as Account to Migration

--- Resources/Scripts/1.1/Set-ImmutableIdForActiveDirectoryUsers.ps1
+++ Resources/Scripts/1.1/Set-ImmutableIdForActiveDirectoryUsers.ps1
@@ -79,21 +79,7 @@ function Set-ImmutableIdForActiveDirectoryUsers {
 
 function Connect-AzureActiveDirectory {
     Try {
-        $requiredAzureModule = "Microsoft.Graph.Users"
-        if (-not (Get-Module -ListAvailable -Name $requiredAzureModule)) {
-            Write-Error "Missing required Azure module '$requiredAzureModule'"
-            Stop-ScriptExecution
-        }
-
-        # default connection name
-        $connectionName = "AzureRunAsConnection"
-
-        $servicePrincipalConnection = Get-AutomationConnection –Name $connectionName  
-
-        write-output "Logging in to Azure AD"
-        Connect-MgGraph -ClientID $servicePrincipalConnection.ApplicationId `
-                -TenantId $servicePrincipalConnection.TenantId `
-                -CertificateThumbprint $servicePrincipalConnection.CertificateThumbprint
+        Connect-MgGraph -Identity
     }
     Catch {
         Write-Error "Failed to establish connection with Azure AD"
