296c18b36b69043d87e8f04e4f96c581bbb353a4
zimam <md-zayed.imam@thalesgroup.com>
2023-06-09 08:53:17 -0400

    Update Version

--- Resources/Scripts/1.1/Set-ImmutableIdForActiveDirectoryUsers.ps1
+++ Resources/Scripts/1.1/Set-ImmutableIdForActiveDirectoryUsers.ps1
@@ -103,8 +103,6 @@ function Connect-AzureActiveDirectory {
     }   
 }
 
-
-
 function Read-UsersBatchWithMissingImmutableId($acceptedBatchSize) {
     $usersWithMissingImmutableId = @()
     $acceptedUpnChars = 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w',
