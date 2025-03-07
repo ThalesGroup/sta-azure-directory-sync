cd99ef2d542f9c28e31ffc2809db3670a32f48c0
Jean-Baptiste Fidelia <38104880+jbfgem@users.noreply.github.com>
2025-03-07 09:48:45 -0500

    Update MSGraphPermissionToManagedIdentity.ps1
    First draft conversion from AzureAD (Deprecated) to MS Graph.

--- Resources/MSGraphPermissionScript/MSGraphPermissionToManagedIdentity.ps1
+++ Resources/MSGraphPermissionScript/MSGraphPermissionToManagedIdentity.ps1
@@ -5,18 +5,18 @@ $ServicePrincipalID = $ObjectId
 $GraphAppId = "00000003-0000-0000-c000-000000000000"
 $PermissionName = "User.ReadWrite.All"
 
-$GraphServicePrincipal = Get-AzureADServicePrincipal -Filter "appId eq '$GraphAppId'"
+$GraphServicePrincipal = Get-MgServicePrincipal -Filter "appId eq '$GraphAppId'"
 $AppRole = $GraphServicePrincipal.AppRoles | Where-Object {$_.Value -eq $PermissionName -and $_.AllowedMemberTypes -contains "Application"}
 
 Try {
-    $MSI = (Get-AzureADServicePrincipal -ObjectId $ServicePrincipalID)
-    $GraphServicePrincipal = Get-AzureADServicePrincipal -Filter "appId eq '$GraphAppId'"
+    $MSI = (Get-MgServicePrincipal -ObjectId $ServicePrincipalID)
+    $GraphServicePrincipal = Get-MgServicePrincipal -Filter "appId eq '$GraphAppId'"
     $AppRole = $GraphServicePrincipal.AppRoles | Where-Object {$_.Value -eq $PermissionName -and $_.AllowedMemberTypes -contains "Application"}
     if ($null -eq $AppRole) { 
         Write-Error "Unable to get scope $scope";
         exit 
     }
-    New-AzureAdServiceAppRoleAssignment -ObjectId $MSI.ObjectId -PrincipalId $MSI.ObjectId -ResourceId $GraphServicePrincipal.ObjectId -Id $AppRole.Id
+    New-MgServicePrincipalAppRoleAssignment -ServicePrincipalId $MSI.ObjectId -AppRoleId $AppRole.Id -PrincipalId $MSI.ObjectId
     Write-Host "Assined Scope $scope"
 } Catch {
     Write-Error $_.Exception.Message
